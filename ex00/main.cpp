2299ad566fdf0a67056665fbfb964ffff0a3ebf0
3ka1tz <ekaitzlopez2025@gmail.com>
2025-12-12 10:51:18 +0100

    subject main applied

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,25 +4,21 @@
 
 int main(void)
 {
-    int a = 1, b = 2;
+    int a = 2;
+    int b = 3;
 
-    ::swap(a, b);
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+    std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
 
-    std::cout << "a = " << a << ";\n";
-    std::cout << "b = " << b << ";\n";
-    std::cout << "min(a, b) = " << ::min(a, b) << ";\n";
-    std::cout << "max(a, b) = " << ::max(a, b) << ";\n";
-
-    std::cout << "\n";
-
-    char c = 'c', d = 'd';
+    std::string c = "chaine1";
+    std::string d = "chaine2";
 
     ::swap(c, d);
-
-    std::cout << "c = " << c << ";\n";
-    std::cout << "d = " << d << ";\n";
-    std::cout << "min(c, d) = " << ::min(c, d) << ";\n";
-    std::cout << "max(c, d) = " << ::max(c, d) << ";\n";
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+    std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
 
     return (0);
 }
