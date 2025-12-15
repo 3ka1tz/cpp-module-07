d727f981ed8877fa5dc0797377daba8a1dd67fcd
3ka1tz <ekaitzlopez2025@gmail.com>
2025-12-15 12:39:08 +0100

    ex02 finished

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,23 +2,18 @@
 
 #include <iostream>
 
-int main(void)
-{
-    int a = 2;
-    int b = 3;
+int main() {
+    int a = 2, b = 3;
 
     ::swap(a, b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
     std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
 
-    std::string c = "chaine1";
-    std::string d = "chaine2";
+    std::string c = "chaine1", d = "chaine2";
 
     ::swap(c, d);
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
     std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
-
-    return (0);
 }
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -2,27 +2,20 @@
 #define WHATEVER_HPP
 
 template <typename T>
-void swap(T& a, T& b)
-{
-    T tmp;
-
-    tmp = a;
+void swap(T& a, T& b) {
+    T tmp = a;
     a = b;
     b = tmp;
-
-    return;
 }
 
 template <typename T>
-const T& min(const T& a, const T& b)
-{
-    return ((a < b) ? a : b);
+const T& min(const T& a, const T& b) {
+    return a < b ? a : b;
 }
 
 template <typename T>
-const T& max(const T& a, const T& b)
-{
-    return ((a > b) ? a : b);
+const T& max(const T& a, const T& b) {
+    return a > b ? a : b;
 }
 
 #endif
