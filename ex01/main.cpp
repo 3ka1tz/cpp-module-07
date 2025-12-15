f885499200e3782a762b9b62f9a996f52d2bb6af
3ka1tz <ekaitzlopez2025@gmail.com>
2025-12-15 12:45:05 +0100

    ex01 finished

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,50 +4,31 @@
 #include <iostream>
 #include <string>
 
-void decrement(int& i)
-{
+void decrement(int& i) {
     --i;
-
-    return;
 }
 
-void printInt(const int& i)
-{
-    std::cout << i << "\n";
-
-    return;
+void printInt(const int& i) {
+    std::cout << i << std::endl;
 }
 
-void toUpper(std::string& s)
-{
+void toUpper(std::string& s) {
     for (size_t i = 0; i < s.length(); ++i)
-    {
         s[i] = std::toupper(s[i]);
-    }
-
-    return;
 }
 
-void printString(const std::string& s)
-{
-    std::cout << s << "\n";
-
-    return;
+void printString(const std::string& s){
+    std::cout << s << std::endl;
 }
 
-int main(void)
-{
+int main() {
     int numbers[3] = {1, 2, 3};
 
-    ::iter(numbers, sizeof(numbers) / sizeof(numbers[0]), decrement);
-    ::iter(numbers, sizeof(numbers) / sizeof(numbers[0]), printInt);
-
-    std::cout << "\n";
+    iter(numbers, sizeof(numbers) / sizeof(numbers[0]), decrement);
+    iter(numbers, sizeof(numbers) / sizeof(numbers[0]), printInt);
 
     std::string names[3] = {"Alice", "Bob", "Charlie"};
 
-    ::iter(names, sizeof(names) / sizeof(names[0]), toUpper);
-    ::iter(names, sizeof(names) / sizeof(names[0]), printString);
-
-    return (0);
+    iter(names, sizeof(names) / sizeof(names[0]), toUpper);
+    iter(names, sizeof(names) / sizeof(names[0]), printString);
 }
